8545062f52a09ec54dd0b29dd6e4141617d1a9b9
zyq <zyq@zyq.lan>
2024-02-01 11:19:08 +0800

    update git

--- git/git.py
+++ git/git.py
@@ -4,11 +4,12 @@
 def print_git_cmd():
     print("git usage command:")
     git_cmd = """
-git add .
-git commit -m "提交信息"
-git push
-git log
-git status
+	git add .
+	git commit -m "提交信息"
+	git push
+	git log
+	git status
+	git pull origin master   #只同步变化的内容
    """
     print(git_cmd)  
 
