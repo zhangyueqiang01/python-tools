36102cfd7c1efc742f6b0576e488ee0df44d8176
zhangyueqiang01 <zhangyueqiang01@gmail.com>
2024-06-25 09:59:23 +0800

    update grub

--- software/software.py
+++ software/software.py
@@ -396,7 +396,7 @@ def print_grub_cmd():
     print("grub usage command:")
     grub_cmd = """
 
-#gurb映像的构成
+# gurb映像的构成
 grub1
 +-----------------------------------------------------------------------+
 | boot.img   |   core.img   |  |   |  modules   |   |  modules  |       |
@@ -416,23 +416,26 @@ core.img
 |            |                 |                         |                                         |                        |
 
 
-#查看mbr
+# 查看mbr
 dd if=/dev/sda of=/tmp/sda_mbr.img count=1 bs=512
 hexdump -C /tmp/sda_mbr.img
 
-#在MBR与MBR后面的空闲空间中安装grub，安装grub1与grub1.5 stage
+# 在MBR与MBR后面的空闲空间中安装grub，安装grub1与grub1.5 stage
 grub2-install --boot-directory=/disk/boot /dev/vdb
 
-#创建grub.cfg文件
+# 创建grub.cfg文件
 grub2-mkconfig -o /boot/grub2/grub.cfg
 
-#grub.cfg文件丢失后进行修复，相关文件可以自动补全
+# efi模式下(GPT分区)grub.cfg文件存放路径
+/boot/efi/EFI/redhat/grub.cfg
+
+# grub.cfg文件丢失后进行修复，相关文件可以自动补全
 ls
 set root='hd0,msdos1'
 linux16 /vmlinuz-3.10.0-514.el7.x86_64 root=/dev/mapper/cl-root
 initrd16 /initramfs-3.10.0-514.el7.x86_64.img
 
-#grub设置密码
+# grub设置密码
 [root@node09 ~]# grub2-setpassword 
 Enter password: 
 Confirm password: 
