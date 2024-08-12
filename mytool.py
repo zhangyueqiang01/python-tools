58c2b9bfe0e84a726c7c3428f186e79147d51e73
zyq <zyq@zyq.lan>
2024-08-12 17:12:00 +0800

    add assembly

--- mytool.py
+++ mytool.py
@@ -152,7 +152,7 @@ def main():
                                            'h3c','dp','huawei1','huawei2','zte','osp','ceph','docker','iptables','tcpdump','route',\
                                             'ip','nmcli','bond','kvm','kcompose','git','vim','bash','find','linuxbasic','dd','awk',\
 					   'sed','ubuntu','echo','grub','initramfs','zip','pam','nova','cinder','neutron','glance',\
-					   'diskio','netio','cputop10','memtop10','login','gpt','iostat','vmstat','iotop','sar','parted','systemd','mod','dracut','mount','pnet','iperf','ascii','hexdump','lamp','objdump','xxxx'], help="Display usage instance")
+					   'diskio','netio','cputop10','memtop10','login','gpt','iostat','vmstat','iotop','sar','parted','systemd','mod','dracut','mount','pnet','iperf','ascii','hexdump','lamp','objdump','assembly','xxxx'], help="Display usage instance")
 
 
     parser.add_argument("-i", "--install", action='store', dest="pkg",help="install packages on remote host")
@@ -301,6 +301,8 @@ def main():
        print_lamp_cmd()
     elif args.show == 'objdump':
        print_objdump_cmd()
+    elif args.show == 'assembly':
+       print_assembly_cmd()
     elif args.show == 'xxxx':
        print_xxxx_cmd()
 
