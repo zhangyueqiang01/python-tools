1cc04c5d0aa83dfc6c011db2b43ce4559ecae881
zyq <zyq@zyq.lan>
2024-10-08 10:08:13 +0800

    add virsh usage

--- mytool.py
+++ mytool.py
@@ -152,7 +152,7 @@ def main():
                                            'h3c','dp','huawei1','huawei2','zte','osp','ceph','docker','iptables','tcpdump','route',\
                                             'ip','nmcli','bond','kvm','kcompose','git','vim','bash','find','linuxbasic','dd','awk',\
 					   'sed','ubuntu','echo','grub','initramfs','zip','pam','nova','cinder','neutron','glance',\
-					   'diskio','netio','cputop10','memtop10','login','gpt','iostat','vmstat','iotop','sar','parted','systemd','mod','dracut','mount','pnet','iperf','ascii','hexdump','lamp','objdump','assembly','dline','ckernel','mysql','nginx','xxxx'], help="Display usage instance")
+					   'diskio','netio','cputop10','memtop10','login','gpt','iostat','vmstat','iotop','sar','parted','systemd','mod','dracut','mount','pnet','iperf','ascii','hexdump','lamp','objdump','assembly','dline','ckernel','mysql','nginx','virsh','xxxx'], help="Display usage instance")
 
 
     parser.add_argument("-i", "--install", action='store', dest="pkg",help="install packages on remote host")
@@ -311,6 +311,8 @@ def main():
        print_mysql_cmd()
     elif args.show == 'nginx':
        print_nginx_cmd()
+    elif args.show == 'virsh':
+       print_virsh_cmd()
     elif args.show == 'xxxx':
        print_xxxx_cmd()
 
