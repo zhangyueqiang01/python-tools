182e210813c0a85d085feef6c7f67b7981cc2459
ECS <you@example.com>
2026-01-23 14:48:12 +0800

    add rootdir info

--- README.txt
+++ README.txt
@@ -112,6 +112,7 @@ docker_file: docker file 解读
 docker_docker0: docker0详解
 ping: ping命令使用帮助
 net: Linux 内核收发网络包流程
+rootdir: Linux系统'/'目录下的目录功能简介
 
 aa --show c --item current : 通过 current 宏查看进程属性的示例代码
 aa --show c --item cpu_consume: cpu 负载打满源代码
--- mytool.py
+++ mytool.py
@@ -160,7 +160,7 @@ def main():
                                            'h3c','dp','huawei1','huawei2','zte','osp','ceph','docker','iptables','tcpdump','route',\
                                             'ip','nmcli','bond','kvm','kcompose','git','vim','bash','find','linuxbasic','dd','awk',\
 					   'sed','ubuntu','echo','grub','initramfs','zip','pam','nova','cinder','neutron','glance',\
-					   'diskio','netio','cputop10','memtop10','login','gpt','iostat','vmstat','iotop','sar','parted','systemd','mod','dracut','mount','pnet','iperf','ascii','hexdump','lamp','objdump','dline','ckernel','mysql','nginx','virsh','chntpw','time','yum','nic','newline','regularExp','nfs','kdump','losetup','xxd','dline2','ethtool','grep','dmesg','gcc','trace','c','lvm','kerneldir','wget','process','strace','chage','reboot','gnu','cpu_register','assembly','boot_fail','cpu_cache','src2bin','core_dump','gdb','top','htop','syscall','syscall_instance','launch_ipv6','shell','scp','interrupt','interrupt_instance','kthread','ps','dmidecode','lspci','sysctl','mm','poetry','keepalived','keepalived_nginx','mlag','ruijie','stp','ftp','english','elf','readelf','task_struct','elf_header','elf_program_header','elf_section_header','lsof','ltrace','smartctl','lsscsi','sub_net','arp','selinux','cpu','cpu_mode','cpu_privilege','cpu_component','mem_wr','maps','smaps','proc','iperf3','storage_unit','eg','fio','uname','crun','shadow','port','ipv6','lan','kpath','chpath','psstat','asm_cmd','cpu_flags','date','s2e','docker_image','docker_file','ansible','docker_docker0','ping','net','xxxx'], help="Display usage instance")
+					   'diskio','netio','cputop10','memtop10','login','gpt','iostat','vmstat','iotop','sar','parted','systemd','mod','dracut','mount','pnet','iperf','ascii','hexdump','lamp','objdump','dline','ckernel','mysql','nginx','virsh','chntpw','time','yum','nic','newline','regularExp','nfs','kdump','losetup','xxd','dline2','ethtool','grep','dmesg','gcc','trace','c','lvm','kerneldir','wget','process','strace','chage','reboot','gnu','cpu_register','assembly','boot_fail','cpu_cache','src2bin','core_dump','gdb','top','htop','syscall','syscall_instance','launch_ipv6','shell','scp','interrupt','interrupt_instance','kthread','ps','dmidecode','lspci','sysctl','mm','poetry','keepalived','keepalived_nginx','mlag','ruijie','stp','ftp','english','elf','readelf','task_struct','elf_header','elf_program_header','elf_section_header','lsof','ltrace','smartctl','lsscsi','sub_net','arp','selinux','cpu','cpu_mode','cpu_privilege','cpu_component','mem_wr','maps','smaps','proc','iperf3','storage_unit','eg','fio','uname','crun','shadow','port','ipv6','lan','kpath','chpath','psstat','asm_cmd','cpu_flags','date','s2e','docker_image','docker_file','ansible','docker_docker0','ping','net','rootdir','xxxx'], help="Display usage instance")
 
 
     parser.add_argument('--item', choices=['main','var', 'printf','macron','struct','vas','mem_consume','cpu_consume','fork','current'], help="Usage: ./mytool.py --show c --item printf")
@@ -548,6 +548,8 @@ def main():
        print_ping_cmd()
     elif args.show == 'net':
        print_net_cmd()
+    elif args.show == 'rootdir':
+       print_rootdir_cmd()
     elif args.show == 'xxxx':
        print_xxxx_cmd()
 
